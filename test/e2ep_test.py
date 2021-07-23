1d2d0f6b399e8a9ea0b86fed8a9e8c9966f11976
Shota Nagayama <shota.nagayama@mercari.com>
2021-07-23 19:04:31 +0900

    implemented test for primitive functions

--- test/e2ep_test.py
+++ test/e2ep_test.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from End2EndPurification.e2ep import BlockingTimes, prepare_nodes_and_links, LocalBellPairProcessor
+from End2EndPurification.e2ep import BlockingTimes, prepare_nodes_and_links, LocalBellPairProcessor, calc_new_fidelity_by_entanglement_swapping, calc_new_fidelity_by_purification, calc_success_rate_of_purification
 
 class NodeTest(TestCase):
     def test_connect_to(self):
@@ -8,10 +8,12 @@ class NodeTest(TestCase):
 class FuncTest(TestCase):
     def test_prepare_nodes_and_links(self):
         fidelity_raw_bellpair = 0.99
-        p_int_node = 0.99
-        p_end_node = 0.99
+        p_op_int_node = 0.99
+        p_mem_int_node = 0.99
+        p_op_end_node = 0.99
+        p_mem_end_node = 0.99
         for num_node in range(2,5):
-            nodes, links = prepare_nodes_and_links(num_node, fidelity_raw_bellpair, p_int_node, p_end_node)
+            nodes, links = prepare_nodes_and_links(num_node, fidelity_raw_bellpair, p_op_int_node, p_mem_int_node, p_op_end_node, p_mem_end_node)
             self.assertEqual(len(nodes), num_node)
             self.assertEqual(len(links), num_node-1)
             for node in nodes[1:-1]:
@@ -21,16 +23,28 @@ class FuncTest(TestCase):
             for node in (nodes[0], nodes[-1]):
                 self.assertEqual(len(node.links), 1)
                 self.assertEqual(len(node.neighbor), 1)
-                self.assertTrue(all([node in n.neighbor for n in node.neighbor]))                
+                self.assertTrue(all([node in n.neighbor for n in node.neighbor]))      
+    def test_calc_fidelity_and_blocking_time(self):
+        pass
+    def test_calc_new_fidelity_by_entanglement_swapping(self):
+        self.assertEqual(calc_new_fidelity_by_entanglement_swapping(0.98, 0.98), 0.98*0.98)
+    def test_calc_new_fidelity_by_purification(self):
+        self.assertEqual(calc_new_fidelity_by_purification(0.99, 0.98), 0.99*0.98 / (0.99 * 0.98 + 0.01 * 0.02))
+    def test_calc_success_rate_of_purification(self):
+        self.assertEqual(calc_success_rate_of_purification(0.99, 0.98), 0.98*0.99+0.01*0.02)
+   
+
 
 class LocalBellPairProcessorTest(TestCase):
     def setUp(self) -> None:
         num_node = 5
-        self.p_int_node = 0.99
-        self.p_end_node = 0.99        
+        self.p_op_int_node = 0.99
+        self.p_mem_int_node = 0.99
+        self.p_op_end_node = 0.99        
+        self.p_mem_end_node = 0.99
         self.fidelity_raw_bellpair = 0.98
         target_fidelity = 0.99
-        self.nodes, self.links = prepare_nodes_and_links(num_node, self.fidelity_raw_bellpair, self.p_int_node, self.p_end_node)
+        self.nodes, self.links = prepare_nodes_and_links(num_node, self.fidelity_raw_bellpair, self.p_op_int_node, self.p_mem_int_node, self.p_op_end_node, self.p_mem_end_node)
         self.local_bpps = []
         for i in range(num_node-1):
             local_bpp = LocalBellPairProcessor(self.nodes[i], self.nodes[i+1], self.links[i])
