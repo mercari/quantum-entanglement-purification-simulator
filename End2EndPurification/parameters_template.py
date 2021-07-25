0f8e4048852b3343ee721515bf3cbacfd83e7d3c
Shota Nagayama <shota.nagayama@mercari.com>
2021-07-25 14:58:29 +0900

    mv parameters.py to parameters_template.py

--- End2EndPurification/parameters_template.py
+++ End2EndPurification/parameters_template.py
@@ -1,5 +1,5 @@
 def parameters():
-    directory_root = '../data2/'
+    directory_root = '../data1/'
     fidelity_raw_bellpair = [0.8]
     layer2_target_fidelity = [0.9, 0.99, 0.999, 0.9999]
     layer3_target_fidelity = [0.9, 0.99, 0.999, 0.9999]
@@ -8,19 +8,8 @@ def parameters():
     p_mem_int_node = [0.01, 0.001, 0.0001]
     p_op_end_node = [0.01, 0.001, 0.0001]
     p_mem_end_node = [0.01, 0.001, 0.0001]
-    num_node = [i for i in range(100, 2001, 100)]
+    num_node = [i for i in range(2, 65)] # [i for i in range(100, 2001, 100)]
     purification_at_int_nodes = [False, True]
-    """
-    fidelity_raw_bellpair = [0.84]
-    layer2_target_fidelity = [0.9, 0.99]
-    layer3_target_fidelity = [0.9, 0.99]
-    layer4_target_fidelity = [0.9, 0.99]
-    p_op_int_node = [0.01, 0.001]
-    p_mem_int_node = [0.01, 0.001]
-    p_op_end_node = [0.01, 0.001]
-    p_mem_end_node = [0.01, 0.001]
-    num_node = [i for i in range(2, 8)]
-    purification_at_int_nodes = [False, True]
-    """
+    
     return directory_root, fidelity_raw_bellpair,layer2_target_fidelity,layer3_target_fidelity,layer4_target_fidelity,p_op_int_node,p_mem_int_node,p_op_end_node,p_mem_end_node,num_node,purification_at_int_nodes
     
