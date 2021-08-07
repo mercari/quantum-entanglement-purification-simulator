d8c32a885d74b21b05e6d4a75ec3e3aeacde583b
Shota Nagayama <shota.nagayama@mercari.com>
2021-08-07 10:01:31 +0900

    functionにばらした

--- End2EndPurification/e2ep.py
+++ End2EndPurification/e2ep.py
@@ -2,6 +2,7 @@
 import sys
 import json
 import math
+from typing import Dict, Tuple
 try:
     from fidelity import Fidelity, fidelity_to_p, p_to_fidelity
 except:
@@ -22,23 +23,45 @@ try:
 except:
     from End2EndPurification.blocking_times import BlockingTimes
 
+class Config:
+    def __init__(self, arg) -> None:
+        if isinstance(arg, Dict):
+            json_dict = arg
+            self.fidelity_raw_bellpair = float(json_dict['fidelity_raw_bellpair'])
+            self.layer2_target_fidelity = float(json_dict['layer2_target_fidelity'])
+            self.layer3_target_fidelity = float(json_dict['layer3_target_fidelity'])
+            self.layer4_target_fidelity = float(json_dict['layer4_target_fidelity'])
+            self.p_op_int_node = float(json_dict['p_op_int_node'])
+            self.p_mem_int_node = float(json_dict['p_mem_int_node'])
+            self.p_op_end_node = float(json_dict['p_op_end_node'])
+            self.p_mem_end_node = float(json_dict['p_mem_end_node'])
+            self.num_node = int(json_dict['num_node'])
+            self.purification_at_int_nodes = bool(json_dict['purification_at_int_nodes'])
+            self.file_out = json_dict['file_out']
+            self.settings = json_dict
+        elif isinstance(arg, Tuple):
+            fidelity_raw_bellpair, layer2_target_fidelity, layer3_target_fidelity, layer4_target_fidelity, p_op_int_node, p_mem_int_node, p_op_end_node, p_mem_end_node, num_node, purification_at_int_nodes, file_out = arg
+            self.fidelity_raw_bellpair = fidelity_raw_bellpair
+            self.layer2_target_fidelity = layer2_target_fidelity
+            self.layer3_target_fidelity = layer3_target_fidelity
+            self.layer4_target_fidelity = layer4_target_fidelity
+            self.p_op_int_node = p_op_int_node
+            self.p_mem_int_node = p_mem_int_node
+            self.p_op_end_node = p_op_end_node
+            self.p_mem_end_node = p_mem_end_node
+            self.num_node = num_node
+            self.purification_at_int_nodes = purification_at_int_nodes
+            self.file_out = file_out
+        else:
+            print("config load error")
+            return False
+
 def load_json(json_file):
     with open(json_file, 'r') as fd:
         json_dict = json.load(fd)
+        config = Config(json_dict)
 
-        fidelity_raw_bellpair = float(json_dict['fidelity_raw_bellpair'])
-        layer2_target_fidelity = float(json_dict['layer2_target_fidelity'])
-        layer3_target_fidelity = float(json_dict['layer3_target_fidelity'])
-        layer4_target_fidelity = float(json_dict['layer4_target_fidelity'])
-        p_op_int_node = float(json_dict['p_op_int_node'])
-        p_mem_int_node = float(json_dict['p_mem_int_node'])
-        p_op_end_node = float(json_dict['p_op_end_node'])
-        p_mem_end_node = float(json_dict['p_mem_end_node'])
-        num_node = int(json_dict['num_node'])
-        purification_at_int_nodes = bool(json_dict['purification_at_int_nodes'])
-        file_out = json_dict['file_out']
-
-    return fidelity_raw_bellpair, layer2_target_fidelity, layer3_target_fidelity, layer4_target_fidelity, p_op_int_node, p_mem_int_node, p_op_end_node, p_mem_end_node, num_node, purification_at_int_nodes, file_out, json_dict
+    return config
 
 def save_json(json_file, link_fidelity, link_bt_int_node, link_bt_end_node, e2e_raw_fidelity, e2e_raw_bt_int_node, e2e_raw_bt_end_node, e2e_final_fidelity, e2e_final_bt_int_node, e2e_final_bt_end_node, settings):
     json_dict = {
@@ -79,45 +102,45 @@ def main():
         num_node = int(sys.argv[9]) # linear network
         purification_at_int_nodes = bool(sys.argv[10])
         file_out = None
-        settings = None
+        config = Config((fidelity_raw_bellpair, layer2_target_fidelity, layer3_target_fidelity, layer4_target_fidelity, p_op_int_node, p_mem_int_node, p_op_end_node, p_mem_end_node, num_node, purification_at_int_nodes, file_out))
 
-        run(fidelity_raw_bellpair, layer2_target_fidelity, layer3_target_fidelity, layer4_target_fidelity, p_op_int_node, p_mem_int_node, p_op_end_node, p_mem_end_node, num_node, purification_at_int_nodes, file_out, settings)
+        run(config)
         return True
     print("Error. Simulation don't run.")
     return False
 
 def run_with_setting_file(setting_file):
-    fidelity_raw_bellpair, layer2_target_fidelity, layer3_target_fidelity, layer4_target_fidelity, p_op_int_node, p_mem_int_node, p_op_end_node, p_mem_end_node, num_node, purification_at_int_nodes, file_out, settings = load_json(setting_file)
+    config = load_json(setting_file)
 
-    run(fidelity_raw_bellpair, layer2_target_fidelity, layer3_target_fidelity, layer4_target_fidelity, p_op_int_node, p_mem_int_node, p_op_end_node, p_mem_end_node, num_node, purification_at_int_nodes, file_out, settings)
+    run(config)
 
-def run(fidelity_raw_bellpair, layer2_target_fidelity, layer3_target_fidelity, layer4_target_fidelity, p_op_int_node, p_mem_int_node, p_op_end_node, p_mem_end_node, num_node, purification_at_int_nodes, file_out, settings):
-    nodes, links = prepare_nodes_and_links(num_node, fidelity_raw_bellpair, p_op_int_node, p_mem_int_node, p_op_end_node, p_mem_end_node)
+def run(config):
+    nodes, links = prepare_nodes_and_links(config)
 
-    output = calc_fidelity_and_blocking_time(nodes, links, layer2_target_fidelity, layer3_target_fidelity, layer4_target_fidelity, purification_at_int_nodes)
+    output = calc_fidelity_and_blocking_time(nodes, links, config)
     if output is False:
-        with open(file_out, 'w') as fd:
-            json.dump({'success': False,'settings': settings}, fd, indent=4)
+        with open(config.file_out, 'w') as fd:
+            json.dump({'success': False,'settings': config.setting}, fd, indent=4)
         return
     #print(output)
-    (link_fidelity, bt_link), (e2e_raw_fidelity, bt_e2e_raw), e2e_final_fidelity, bt_e2e_final = output
+    (link_fidelity, bt_link), (e2e_raw_fidelity, bt_e2e_raw), (e2e_final_fidelity, bt_e2e_final) = output
     link_bt_int_node, link_bt_end_node = bt_link.blocking_time_int_node, bt_link.blocking_time_end_node
     e2e_raw_bt_int_node, e2e_raw_bt_end_node = bt_e2e_raw.blocking_time_int_node, bt_e2e_raw.blocking_time_end_node
     e2e_final_bt_int_node, e2e_final_bt_end_node = bt_e2e_final.blocking_time_int_node, bt_e2e_final.blocking_time_end_node
-    if file_out:
-        save_json(file_out, link_fidelity, link_bt_int_node, link_bt_end_node, e2e_raw_fidelity, e2e_raw_bt_int_node, e2e_raw_bt_end_node, e2e_final_fidelity, e2e_final_bt_int_node, e2e_final_bt_end_node, settings)
+    if config.file_out:
+        save_json(config.file_out, link_fidelity, link_bt_int_node, link_bt_end_node, e2e_raw_fidelity, e2e_raw_bt_int_node, e2e_raw_bt_end_node, e2e_final_fidelity, e2e_final_bt_int_node, e2e_final_bt_end_node, config.settings)
 
-def prepare_nodes_and_links(num_node, fidelity_raw_bellpair, p_op_int_node, p_mem_int_node, p_op_end_node, p_mem_end_node):
-    nodes = [Node(True, p_op_end_node, p_mem_end_node)] + [Node(False, p_op_int_node, p_mem_int_node) for _ in range(num_node-2)] + [Node(True, p_op_end_node, p_mem_end_node)]
+def prepare_nodes_and_links(config):
+    nodes = [Node(True, config.p_op_end_node, config.p_mem_end_node)] + [Node(False, config.p_op_int_node, config.p_mem_int_node) for _ in range(config.num_node-2)] + [Node(True, config.p_op_end_node, config.p_mem_end_node)]
     links = []
-    for i in range(num_node-1):
-        link = Link(fidelity_raw_bellpair)
+    for i in range(config.num_node-1):
+        link = Link(config.fidelity_raw_bellpair)
         links.append(link)
         nodes[i].connect_to(nodes[i+1], link)
         link.connect(nodes[i], nodes[i+1])
     return nodes, links
 
-def calc_fidelity_and_blocking_time(nodes, links, layer2_target_fidelity, layer3_target_fidelity, layer4_target_fidelity, purification_at_int_nodes):
+def calc_fidelity_and_blocking_time(nodes, links, config):
     bpp_single_layer = []
     bpp_all_layers = [bpp_single_layer]
 
@@ -126,13 +149,38 @@ def calc_fidelity_and_blocking_time(nodes, links, layer2_target_fidelity, layer3
         local_bpp = LocalBellPairProcessor(nodes[i], nodes[i+1], links[i])
         bpp_single_layer.append(local_bpp)
     
-    # process Layer2 bpp
+    # process Layer2 bpp (each link bpp)
+    if not process_layer2_bpp(bpp_all_layers, config.layer2_target_fidelity):
+        return False
+    example_l2 = bpp_all_layers[0][0].fidelity, bpp_all_layers[0][0].blocking_times
+
+    # process Layer3 bpp
+    if not process_layer3_bpp(bpp_all_layers, config.purification_at_int_nodes, config.layer3_target_fidelity):
+        return False
+    example_l3 = bpp_all_layers[-1][0].fidelity, bpp_all_layers[-1][0].blocking_times
+
+    # process Layer4 bpp (End-to-End bpp)
+    if not process_layer4_bpp(bpp_all_layers, config.layer4_target_fidelity):
+        return False
+    result = bpp_all_layers[-1][0].fidelity, bpp_all_layers[-1][0].blocking_times
+
+    return example_l2, example_l3, result
+
+def process_layer4_bpp(bpp_all_layers, layer4_target_fidelity):
+    bpp = bpp_all_layers[-1][-1]
+    if not bpp.repeat_purification_until_target_fidelity(layer4_target_fidelity):
+        return False
+    return True
+
+def process_layer2_bpp(bpp_all_layers, layer2_target_fidelity):
+    bpp_single_layer = bpp_all_layers[0]
     for local_bpp in bpp_single_layer:
         if not local_bpp.repeat_purification_until_target_fidelity(layer2_target_fidelity):
             return False
-        tmp1 = local_bpp.fidelity, local_bpp.blocking_times
+    return True
 
-    # process Layer3 bpp
+def process_layer3_bpp(bpp_all_layers, purification_at_int_nodes, layer3_target_fidelity):
+    bpp_single_layer = bpp_all_layers[-1]
     while True:
         res, bpp_single_layer = process_layer3_bpp_one_round(bpp_single_layer, purification_at_int_nodes, layer3_target_fidelity)
 
@@ -144,14 +192,7 @@ def calc_fidelity_and_blocking_time(nodes, links, layer2_target_fidelity, layer3
         if bpp_single_layer.__len__() == 1:
             # End-to-End bpp is achieved
             break
-
-    # process Layer4 bpp (End-to-End bpp)
-    bpp = bpp_all_layers[-1][-1]
-    tmp2 = bpp.fidelity, bpp.blocking_times
-    if not bpp.repeat_purification_until_target_fidelity(layer4_target_fidelity):
-        return False
-
-    return tmp1, tmp2, bpp.fidelity, bpp.blocking_times
+    return True
 
 def process_layer3_bpp_one_round(bpp_single_layer_prev, purification_at_int_nodes, layer3_target_fidelity):
     bpp_single_layer = []
